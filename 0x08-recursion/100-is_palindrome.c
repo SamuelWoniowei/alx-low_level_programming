35439bfe4fcb4908ea6aad137b1853beb26bf549
Samuel Blessed Woniowei <xcellentsam@gmail.com>
2022-06-25 05:05:47 -0700

    C recursion exercise

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,58 @@
+100-is_palindrome.c
+@esube1000
+esube1000 Create 100-is_palindrome.c
+Latest commit 672d2b0 on Mar 31
+ History
+ 1 contributor
+50 lines (42 sloc)  607 Bytes
+
+#include "main.h"
+
+/**
+ * _strlen_recursion - size
+ * @s: pointer to string params
+ * Return: recursion
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
+}
+
+/**
+ * p1 - palindrome
+ * @s: pointer to string
+ * @l: position
+ * Return: boolena
+ */
+
+int p1(char *s, int l)
+{
+	if (l < 1)
+	{
+		return (1);
+	}
+
+	if (*s == *(s + l))
+	{
+		return (p1(s + 1, l - 2));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - palindrome
+ * @s: pointer to string
+ * Return: recursion
+ */
+
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	return (p1(s, len - 1));
+}
